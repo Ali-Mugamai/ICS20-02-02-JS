c9a44d4529fef7a0897bed845073c3221c99b662
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-14 14:26:29 -0400

    please wortk

--- js/script.js
+++ js/script.js
@@ -8,17 +8,6 @@
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("perimeter").innerHTML =
-<<<<<<< HEAD
-    "<p>2(3+5)+2(3+5) =  " + (16*2) + "</p>"
-=======
-    "<p>2(3+5)+2(3+5) =  " + (3 + 5 * 2 + 3 + 5 * 2) + "</p>"
->>>>>>> 69704c7a7ced7a1b7e25c732902543580f3d56ba
-  document.getElementById("area of square").innerHTML =
-<<<<<<< HEAD
-    "<p>3 x 5 = " + (5 * 3) + "</p>"
-
-=======
-    "<p>3 x 4 = " + (5 * 3) + "</p>"
->>>>>>> 69704c7a7ced7a1b7e25c732902543580f3d56ba
+  document.getElementById("perimeter").innerHTML = "<p>2(3+5)+2(3+5) =  " + (16*2) + "</p>"
+  document.getElementById("area of square").innerHTML ="<p>3 x 5 = " + (5 * 3) + "</p>"
 }
